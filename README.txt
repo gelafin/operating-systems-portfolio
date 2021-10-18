2e7c99360bd696b63483a8b8aac0db4d2608398a
Mark Mendez <mendemar@os1.engr.oregonstate.edu>
2021-10-18 16:45:12 -0700

    initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+To create the executable "smallsh" from the source files, run the following command, which will use the Makefile in this folder to build the executable:
+    make
---
+++ main.c
@@ -0,0 +1,15 @@
+// If you are not compiling with the gcc option --std=gnu99, then
+// #define _GNU_SOURCE or you might get a compiler warning
+#define _GNU_SOURCE
+#include "./smallsh.h"
+
+/*
+*   Runs an interactive shell program
+*   Compile the program as follows:
+*       gcc --std=gnu99 -o smallsh main.c
+*/
+int main(int argc, char* argv[]) {
+    
+
+    return EXIT_SUCCESS;
+}
